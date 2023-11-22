b3330b918574c63e0372e9e376d8b0c484d2becd
Mauritania-skz <141859319+Mauritania-skz@users.noreply.github.com>
2023-11-22 14:46:52 -0600

    Update __init__.py

--- minimal/__init__.py
+++ minimal/__init__.py
@@ -6,3 +6,14 @@ def my_sum(x, y):
 def my_mul(x, y):
     """A function that multiply. """
     return x*y
+def multiplo(n):
+    return n%5==0
+
+def fibonacci(n):
+    fibs = []
+    a, b = 0, 1
+    while a < n:
+         if multiplo(a):
+            fibs.append(a)
+         a, b = b, a+b
+    return fibs
